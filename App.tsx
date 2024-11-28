5e060d08a398d9435e6e45bf999a32089b4dd1db
geoffroy maidou <maidougeoffroyjaphet@gmail.com>
2024-11-28 17:16:20 +0000

    install tailwindCss

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Nav  from "./Nav";
 import Header from "./Header";
+import Hello from "./src/Hello"
 
 export default function App() {
   return (
     <View style={styles.container}>
 
+      <Hello></Hello>
       <Header></Header>
       <Text>Hello world </Text>
       <Nav></Nav>
